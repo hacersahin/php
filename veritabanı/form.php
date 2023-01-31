0b8ee68fc11cabbdfbd287e7b7f13beec48998c3
Hacer Şahin <120566057+hacersahin@users.noreply.github.com>
2023-01-31 14:15:10 +0300

    Add files via upload
    veri tabanını web sayfasına bağlayıp kullanıcı girişini veri tabanına aktarıyoruz

---
+++ veritabanı/form.php
@@ -0,0 +1,36 @@
+﻿<?php
+//echo $_GET ['email'];
+
+   // print_r($_GET); //içerisindeki elemanlrı yazdırıyor.
+$servername="localhost";
+
+$username="root";
+
+$password="";
+
+$dbname="100ky";
+
+$conn= new mysqli($servername,$username,$password,$dbname);
+
+if( $conn->connect_error){
+
+	die("connection faild: " . $conn->connect_error);
+
+}
+
+$email=$_POST['email'];
+$sifre=$_POST['sifre1'];
+
+$sql="INSERT INTO kullanici VALUES ('','$email','$sifre')";
+
+if($conn->query($sql)== TRUE){
+
+    echo "Yeni Kayıt Oluşturuldu.";
+}
+else {
+
+    echo "Hata" . $conn->error;
+}
+
+$conn->close();
+?>
